7d4884ffb37fa21458594e79a62f46ff7a3a5922
Kevin Berry <xckjb88@gmail.com>
2010-07-06 19:40:42 -0400

    Added rserve and docs.

---
+++ rserve/rserve-init.R
@@ -0,0 +1,10 @@
+##Pre-loaded libraries to speed things up
+
+library("RPostgreSQL")
+library("DBI")
+library("ggplot2")
+library("proto")
+library("grid")
+library("reshape")
+library("plyr")
+library("digest")
