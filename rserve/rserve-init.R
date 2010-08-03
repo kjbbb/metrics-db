acba65256fcba7b2599abac354b4dc4004a7c81f
Kevin Berry <xckjb88@gmail.com>
2010-08-03 17:46:00 -0400

    Added bandwidth versions pie chart.

--- rserve/rserve-init.R
+++ rserve/rserve-init.R
@@ -437,9 +437,41 @@ plot_bandwidth_guardexit_piechart <- function(start, end, path) {
     geom_bar() +
     scale_y_continuous(name="") +
     scale_x_discrete(name="") +
-    scale_colour_brewer(name="Guard/exit flags") +
+    scale_fill_brewer(name="Guard/exit flags") +
     coord_polar("y") +
     opts(title="Bandwidth distribution per guard/exit/relay flags")
+
+  ggsave(filename=path, width=8, height=5, dpi=72)
+
+  #Close database connection
+  dbDisconnect(con)
+  dbUnloadDriver(drv)
+}
+
+plot_bandwidth_versions_piechart <- function(start, end, path) {
+
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, user=dbuser, password=dbpassword, dbname=db)
+
+  q <- paste("select sum(d.bandwidthavg) as bandwidthsum, ",
+    "    substring(d.platform, 5, 5) as version ",
+    "from descriptor d ",
+    "join statusentry s on d.descriptor=s.descriptor ",
+    "where d.bandwidth is not null ",
+    "    and date(s.validafter) >= '",start,"' ",
+    "    and date(s.validafter) <= '",end,"' ",
+    "group by substring(d.platform, 5, 5)", sep="")
+
+  rs <- dbSendQuery(con, q)
+  bandwidth <- fetch(rs,n=-1)
+
+  ggplot(bandwidth, aes(x="", y=bandwidthsum, fill=version)) +
+    geom_bar(position="dodge") +
+    scale_y_continuous(name="") +
+    scale_x_discrete(name="Version") +
+    scale_fill_brewer(name="Version") +
+    coorc_polar("y") +
+    opts(title="Bandwidth distribution per version")
 
   ggsave(filename=path, width=8, height=5, dpi=72)
 
