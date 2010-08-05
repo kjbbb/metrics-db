4053259dd53098746cd12ec7e409fe1b75d7edcb
Kevin Berry <xckjb88@gmail.com>
2010-08-05 00:46:22 -0400

    Changed syntax on the refresh functions.

--- db/matviewrefresh.sql
+++ db/matviewrefresh.sql
@@ -2,9 +2,9 @@
 ----This script should be run every time ernie is run to
 ----keep data sinks up to date.
 
-PERFORM refresh_network_size
-PERFORM refresh_relay_platforms
-PERFORM refresh_relay_versions
-PERFORM refresh_relay_uptime
-PERFORM refresh_relay_bandwidth
-PERFORM refresh_total_bandwidth
+SELECT * FROM refresh_network_size()
+SELECT * FROM refresh_relay_platforms()
+SELECT * FROM refresh_relay_versions()
+SELECT * FROM refresh_relay_uptime()
+SELECT * FROM refresh_relay_bandwidth()
+SELECT * FROM refresh_total_bandwidth()
