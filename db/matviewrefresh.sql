6ea992a5c4fd38a481ec41aecb2318e7a2a08eab
Kevin Berry <xckjb88@gmail.com>
2010-08-04 21:38:54 -0400

    Call the refresh functions from the refresh script.

--- db/matviewrefresh.sql
+++ db/matviewrefresh.sql
@@ -2,4 +2,9 @@
 ----This script should be run every time ernie is run to
 ----keep data sinks up to date.
 
-
+PERFORM refresh_network_size
+PERFORM refresh_relay_platforms
+PERFORM refresh_relay_versions
+PERFORM refresh_relay_uptime
+PERFORM refresh_relay_bandwidth
+PERFORM refresh_total_bandwidth
