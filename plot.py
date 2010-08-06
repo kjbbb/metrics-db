d34913db7777f80d567e1f439a06a8e6ec99978a
Kevin Berry <xckjb88@gmail.com>
2010-08-06 18:49:05 -0400

    Some year graphs temp.

--- plot.py
+++ plot.py
@@ -4,8 +4,16 @@ import pyRserve
 from datetime import date, timedelta
 import hashlib
 import sys
+import pg
+
+dbhost = "localhost"
+dbuser = "ernie"
+dbpassword = ""
+db = "tordir"
 
 conn = pyRserve.rconnect()
+dbconn = pg.connect(dbname=dbuser, dbhost=dbhost, \
+    dbpasswd=dbpassword, dbname=db)
 
 datefmt = "%Y-%m-%d"
 basepath = "/tmp/ernie/"
@@ -14,7 +22,14 @@ networksize = "plot_networksize_line('%s','%s','%s')"
 graphs = ["networksize", "versions", "platforms", "bandwidth", "uptime",
     "gettor", "torperf"]
 
+#Find years, min and max ranges.
 ranges = ["30", "90", "180"]
+
+yearquery = "select extract(year from date(date)) " +
+            "from network_size " +
+            "group by extract(year from date(date))"
+
+dbconn.query(yearquery)
 
 for r in ranges:
     end = date.today()
@@ -24,6 +39,7 @@ for r in ranges:
            + "-" + str(end)).hexdigest() + ".png"
 
     networksize_q = networksize % (start, end, path)
+
     #Query Rserve
     conn(networksize_q)
 
