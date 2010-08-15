ea4cb2639633b4ce6921ad3cd31cded47458088f
Kevin Berry <xckjb88@gmail.com>
2010-08-15 01:50:01 -0400

    Fixed formatting on refresh functions.

--- db/tordir.sql
+++ db/tordir.sql
@@ -320,9 +320,39 @@ CREATE OR REPLACE FUNCTION refresh_network_size() RETURNS INTEGER AS $$
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION refresh_relay_platforms() RETURNS INTEGER AS $$
-        INSERT INTO relay_platforms
-        (date, avg_linux, avg_darwin, avg_bsd, avg_windows, avg_other)
-        SELECT DATE(validafter),
+    BEGIN
+    INSERT INTO relay_platforms
+    (date, avg_linux, avg_darwin, avg_bsd, avg_windows, avg_other)
+    SELECT DATE(validafter),
+        SUM(CASE WHEN platform LIKE '%Linux%' THEN 1 ELSE 0 END) /
+            relay_statuses_per_day.count AS avg_linux,
+        SUM(CASE WHEN platform LIKE '%Darwin%' THEN 1 ELSE 0 END) /
+            relay_statuses_per_day.count AS avg_darwin,
+        SUM(CASE WHEN platform LIKE '%BSD%' THEN 1 ELSE 0 END) /
+            relay_statuses_per_day.count AS avg_bsd,
+        SUM(CASE WHEN platform LIKE '%Windows%' THEN 1 ELSE 0 END) /
+            relay_statuses_per_day.count AS avg_windows,
+        SUM(CASE WHEN platform NOT LIKE '%Windows%'
+            AND platform NOT LIKE '%Darwin%'
+            AND platform NOT LIKE '%BSD%'
+            AND platform NOT LIKE '%Linux%' THEN 1 ELSE 0 END) /
+            relay_statuses_per_day.count AS avg_other
+    FROM descriptor LEFT JOIN statusentry
+    On statusentry.descriptor = descriptor.descriptor
+    JOIN (SELECT COUNT(*) AS count, DATE(validafter) AS date
+            FROM (SELECT DISTINCT validafter FROM statusentry) distinct_consensuse
+            GROUP BY DATE(validafter)) relay_statuses_per_day
+    ON DATE(validafter) = relay_statuses_per_day.date
+    WHERE DATE(validafter) NOT IN (SELECT DATE(date) FROM relay_platforms)
+    GROUP BY DATE(validafter), relay_statuses_per_day.count;
+
+   UPDATE relay_platforms
+   SET avg_linux=new_rp.avg_linux,
+       avg_darwin=new_rp.avg_darwin,
+       avg_windows=new_rp.avg_windows,
+       avg_bsd=new_rp.avg_bsd,
+       avg_other=new_rp.avg_other
+   FROM (SELECT DATE(validafter),
             SUM(CASE WHEN platform LIKE '%Linux%' THEN 1 ELSE 0 END) /
                 relay_statuses_per_day.count AS avg_linux,
             SUM(CASE WHEN platform LIKE '%Darwin%' THEN 1 ELSE 0 END) /
@@ -337,48 +367,20 @@ CREATE OR REPLACE FUNCTION refresh_relay_platforms() RETURNS INTEGER AS $$
                 AND platform NOT LIKE '%Linux%' THEN 1 ELSE 0 END) /
                 relay_statuses_per_day.count AS avg_other
         FROM descriptor LEFT JOIN statusentry
-        On statusentry.descriptor = descriptor.descriptor
+        ON statusentry.descriptor = descriptor.descriptor
         JOIN (SELECT COUNT(*) AS count, DATE(validafter) AS date
                 FROM (SELECT DISTINCT validafter FROM statusentry) distinct_consensuse
                 GROUP BY DATE(validafter)) relay_statuses_per_day
         ON DATE(validafter) = relay_statuses_per_day.date
-        WHERE DATE(validafter) NOT IN (SELECT DATE(date) FROM relay_platforms)
-        GROUP BY DATE(validafter), relay_statuses_per_day.count;
-
-       UPDATE relay_platforms
-       SET avg_linux=new_rp.avg_linux,
-           avg_darwin=new_rp.avg_darwin,
-           avg_windows=new_rp.avg_windows,
-           avg_bsd=new_rp.avg_bsd,
-           avg_other=new_rp.avg_other
-       FROM (SELECT DATE(validafter),
-                SUM(CASE WHEN platform LIKE '%Linux%' THEN 1 ELSE 0 END) /
-                    relay_statuses_per_day.count AS avg_linux,
-                SUM(CASE WHEN platform LIKE '%Darwin%' THEN 1 ELSE 0 END) /
-                    relay_statuses_per_day.count AS avg_darwin,
-                SUM(CASE WHEN platform LIKE '%BSD%' THEN 1 ELSE 0 END) /
-                    relay_statuses_per_day.count AS avg_bsd,
-                SUM(CASE WHEN platform LIKE '%Windows%' THEN 1 ELSE 0 END) /
-                    relay_statuses_per_day.count AS avg_windows,
-                SUM(CASE WHEN platform NOT LIKE '%Windows%'
-                    AND platform NOT LIKE '%Darwin%'
-                    AND platform NOT LIKE '%BSD%'
-                    AND platform NOT LIKE '%Linux%' THEN 1 ELSE 0 END) /
-                    relay_statuses_per_day.count AS avg_other
-            FROM descriptor LEFT JOIN statusentry
-            ON statusentry.descriptor = descriptor.descriptor
-            JOIN (SELECT COUNT(*) AS count, DATE(validafter) AS date
-                    FROM (SELECT DISTINCT validafter FROM statusentry) distinct_consensuse
-                    GROUP BY DATE(validafter)) relay_statuses_per_day
-            ON DATE(validafter) = relay_statuses_per_day.date
-            WHERE DATE(validafter) IN (SELECT DISTINCT date FROM updates)
-            GROUP BY DATE(validafter), relay_statuses_per_day.count) as new_rp
-        WHERE new_rp.date=relay_platforms.date;
+        WHERE DATE(validafter) IN (SELECT DISTINCT date FROM updates)
+        GROUP BY DATE(validafter), relay_statuses_per_day.count) as new_rp
+    WHERE new_rp.date=relay_platforms.date;
     RETURN 1;
     END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION refresh_relay_versions() RETURNS INTEGER AS $$
+    BEGIN
     INSERT INTO relay_versions
     (date, "0.1.2", "0.2.0", "0.2.1", "0.2.2")
     SELECT DATE(validafter),
@@ -428,6 +430,7 @@ CREATE OR REPLACE FUNCTION refresh_relay_versions() RETURNS INTEGER AS $$
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION refresh_total_bandwidth() RETURNS INTEGER AS $$
+    BEGIN
     INSERT INTO total_bandwidth
     (bwavg, bwburst, bwobserved, date)
     SELECT (SUM(bandwidthavg)
@@ -465,7 +468,7 @@ CREATE OR REPLACE FUNCTION refresh_total_bandwidth() RETURNS INTEGER AS $$
         ON DATE(validafter) = relay_statuses_per_day.date
         WHERE DATE(validafter) IN (SELECT DISTINCT date FROM updates)
         GROUP BY DATE(validafter), relay_statuses_per_day.count) as new_tb
-    WHERE new_db.date = total_bandwidth.date;
+    WHERE new_tb.date = total_bandwidth.date;
 
     RETURN 1;
     END;
