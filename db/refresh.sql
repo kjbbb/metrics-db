906af164f05fd7c134fc4c65c5dc3bb51a351315
Kevin Berry <xckjb88@gmail.com>
2010-08-16 11:30:15 -0400

    Reorganized refresh script so everything is in the right order.

--- db/refresh.sql
+++ db/refresh.sql
@@ -2,6 +2,11 @@
 -- This script should be run every time ernie is run to keep data sinks
 -- up to date by calling the refresh functions.
 
+-- Keep the relay_statuses_per_day helper materialized-view up-to-date.
+SELECT *
+INTO relay_statuses_per_day
+FROM relay_statuses_per_day_v;
+
 SELECT * FROM refresh_network_size();
 SELECT * FROM refresh_relay_platforms();
 SELECT * FROM refresh_relay_versions();
@@ -10,11 +15,6 @@ SELECT * FROM refresh_relay_bandwidth();
 SELECT * FROM refresh_total_bandwidth();
 SELECT * FROM refresh_platforms_uptime_month();
 SELECT * FROM refresh_churn();
-
--- Keep the relay_statuses_per_day helper materialized-view up-to-date.
-SELECT *
-INTO relay_statuses_per_day
-FROM relay_statuses_per_day_v;
 
 -- Clear the updates table, since we have just updated everything.
 DELETE FROM updates;
