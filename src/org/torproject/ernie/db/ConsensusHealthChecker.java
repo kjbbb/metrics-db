3546dfb1998097ba2789bc8a9fb40f46506a4da5
Karsten Loesing <karsten.loesing@gmx.net>
2010-09-01 11:32:02 +0200

    Add relay search page to navigation.

--- src/org/torproject/ernie/db/ConsensusHealthChecker.java
+++ src/org/torproject/ernie/db/ConsensusHealthChecker.java
@@ -405,6 +405,7 @@ public class ConsensusHealthChecker {
           + "            <br/>\n"
           + "            <font size=\"2\">\n"
           + "              <a href=\"exonerator.html\">ExoneraTor</a>\n"
+          + "              <a href=\"relaysearch.html\">Relay Search</a>\n"
           + "              <a class=\"current\">Consensus Health</a>\n"
           + "              <a href=\"log.html\">Last Log</a>\n"
           + "            </font>\n"
@@ -825,11 +826,26 @@ public class ConsensusHealthChecker {
       bw.write("      </div>\n"
           + "    </div>\n"
           + "    <div class=\"bottom\" id=\"bottom\">\n"
+          + "      <p>This material is supported in part by the "
+            + "National Science Foundation under Grant No. "
+            + "CNS-0959138. Any opinions, finding, and conclusions "
+            + "or recommendations expressed in this material are "
+            + "those of the author(s) and do not necessarily reflect "
+            + "the views of the National Science Foundation.</p>\n"
           + "      <p>\"Tor\" and the \"Onion Logo\" are <a "
             + "href=\"https://www.torproject.org/trademark-faq.html"
             + ".en\">"
           + "registered trademarks</a> of The Tor Project, "
             + "Inc.</p>\n"
+          + "      <p>Data on this site is freely available under a "
+            + "<a href=\"http://creativecommons.org/publicdomain/"
+            + "zero/1.0/\">CC0 no copyright declaration</a>: To the "
+            + "extent possible under law, the Tor Project has waived "
+            + "all copyright and related or neighboring rights in "
+            + "the data. Graphs are licensed under a <a "
+            + "href=\"http://creativecommons.org/licenses/by/3.0/"
+            + "us/\">Creative Commons Attribution 3.0 United States "
+            + "License</a>.</p>\n"
           + "    </div>\n"
           + "  </body>\n"
           + "</html>");
@@ -839,3 +855,4 @@ public class ConsensusHealthChecker {
     }
   }
 }
+
